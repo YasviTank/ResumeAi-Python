335061e9ac0e547c654bf46987168c988f2f3106
Yasvi Tank <yasvitank@gmail.com>
2025-08-13 00:12:34 +0530

    added streamlit configure code

--- main.py
+++ main.py
@@ -6,3 +6,9 @@ from openai import OpenAI
 from dotenv import load_dotenv
 
 load_dotenv
+
+#Allows us to configure the name of the page
+st.set_page_config(page_title="Resume AI Critiquer", page_icon="📃", layout="centered")
+
+st.title("AI Resume Critiquer")
+st.markdown("Upload your resume and get an AI-powered feedback tailored to your needs!")
